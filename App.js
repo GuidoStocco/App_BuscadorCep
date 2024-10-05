032bdd8c06edb0727051f2073d848564a25050ca
unknown <guidostoccodev@gmail.com>
2024-10-05 19:44:52 -0300

    exportando a Api, criando TextInput para o cep

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react'
+import { StyleSheet, Text, View, SafeAreaView, TextInput, TouchableOpacity} from 'react-native';
+
+import Api from './src/services/Api';
+
 
 export default function App() {
+
+  const [cep, setCep] = useState('')
+
   return (
-    <View style={styles.container}>
-      <Text>Guido Stocco</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.view}>
+        <Text>Digite o cep desejado</Text>
+        <TextInput
+          style={styles.input} placeholder='Ex: 45980000'
+          value={cep}
+          onChangeText={(text) => setCep(text)}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 35
   },
+  view:{
+    alignItems: 'center'
+  },
+  input:{
+
+  }
 });
--- src/services/Api.js
+++ src/services/Api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
-export const Api = axios.create({
+const Api = axios.create({
     baseURL: 'https://viacep.com.br/ws'
 });
+
+export default Api;
