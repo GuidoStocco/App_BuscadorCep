e51bfbe9067ed55e6b449688c9c3ed5244ccaff4
unknown <guidostoccodev@gmail.com>
2024-10-06 20:14:29 -0300

    fixing code

--- App.js
+++ App.js
@@ -1,98 +1,48 @@
 import React, {useState} from 'react';
-import {View, Text, SafeAreaView, StyleSheet, TextInput, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, TextInput, ToucheableOpacity, SafeAreaView} from 'react-native';
 
-import Api from './src/services/Api';
+import Api from './src/services/Api'
 
 export default function App(){
 
-  const [cep, setCep] = useState('')
+    const [cep, setCep] = useState('')
 
-  return( 
-    <SafeAreaView style={StyleSheet.container}>
-      <View style={styles.area}>
-        <Text style={styles.areaText}>Digite o cep desejado</Text>
-      </View>
+    return(
+        <SafeAreaView style={StyleSheet.container}>
+            <View style={styles.view}>
+                <Text style={styles.viewText}>Digite o cep desejado</Text>
 
-      <View style={styles.areaInput}>
-        <TextInput
-        style={styles.input} placeholder='EX : 45980000'
-        value={cep} onChangeText={(text) => setCep(text)}
-        keyboardType='numeric'
-      />
-      </View>
-
-      <View style={styles.buttonArea}>
-          <TouchableOpacity style={[styles.btn, , {backgroundColor: '#ff1a1a'}]}>
-            <Text style={styles.textbtn}>BUSCAR</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={[styles.btn, {backgroundColor: '#1a1aff'}]}>
-            <Text style={styles.textbtn}>LiMPAR</Text>
-          </TouchableOpacity>  
-      </View>
-
-      <View style={styles.areaResultado}>
-        <Text style={styles.resultado}>CEP: 45980000</Text>
-        <Text style={styles.resultado}>LOGRADOURO: 45980000</Text>
-        <Text style={styles.resultado}>CIDADE: 45980000</Text>
-        <Text style={styles.resultado}>ESTADO: 45980000</Text>
-        <Text style={styles.resultado}>CEP: 45980000</Text>
-      </View>
-    </SafeAreaView>
-  );
+                <TextInput
+                    style={styles.input} placeholder='EX: 45980000'
+                    value={cep} onChangeText={(text) => setCep(text)} keyboardType='numeric'
+                />
+            </View>
+        </SafeAreaView>
+    );
 };
 
 
 const styles = StyleSheet.create({
-  container:{
-    flex: 1,
-  },
-  area:{
-    marginTop: 40,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom: 15
-  },
-  areaText:{
-    fontSize: 22,
-    fontWeight: 'bold'
-  },
-  areaInput:{
-    justifyContent:'center',
-    alignItems: 'center'
-  },
-  input:{
-    width: '90%',
-    borderWidth: 1,
-    backgroundColor: '#fff',
-    borderColor: '#ddd',
-    elevation: 1,
-    padding: 10,
-    fontSize: 18
-  },
-  buttonArea:{
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    marginTop: 30,
-  },
-  btn:{
-    padding: 10,
-    borderRadius: 5,
-    paddingLeft: 20,
-    paddingRight: 20
-  },
-  textbtn:{
-    color: '#fff',
-    fontSize: 18,
-    fontWeight: '500'
-  },
-  areaResultado:{
-    marginTop: 50,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  resultado:{
-    fontSize: 22,
-    
-  }
+    container:{
+        flex: 1
+    },
+    view:{
+        marginTop: 40,
+        justifyContent:'center',
+        alignItems: 'center'
+    },
+    viewText:{
+        fontSize: 22,
+        fontWeight: 'bold', 
+        marginBottom: 18
+    },
+    input:{
+        width: '90%',
+        borderWidth: 1,
+        borderColor: '#ddd',
+        backgroundColor: '#fff',
+        padding: 10,
+        fontSize: 18,
+        elevation: 1
+    }
 })
